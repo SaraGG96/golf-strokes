23fe39cecb081e6bbb20bf9ac1b0d4f1a7a773e8
Sara <saragarciagarrido96@gmail.com>
2026-03-26 11:26:39 +0100

    Retirar comentarios

--- src/test/java/edu/teamrocket/golfstrokes/StrokePlayTest.java
+++ src/test/java/edu/teamrocket/golfstrokes/StrokePlayTest.java
@@ -1,7 +1,6 @@
 package edu.teamrocket.golfstrokes;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -15,20 +14,16 @@ public class StrokePlayTest {
 
     @BeforeEach
     void setUp() {
-        // Inicializar objetos necesarios
         scoreCard = new ScoreCard();
         course = new Course();
         strokePlay = new StrokePlay();
         
-        // Crear jugadores
         playerA = new Player("PA", (byte)5);
         playerB = new Player("PB", (byte)10);
         
-        // Añadir jugadores a la scoreCard
         scoreCard.setPlayerA(playerA);
         scoreCard.setPlayerB(playerB);
         
-        // Crear recorridos de ejemplo (71 y 73 golpes)
         Byte[] coursePlayerA = {
             4, 4, 3, 4, 4, 4, 3, 4, 5,  // Out nine: 35
             4, 4, 3, 4, 4, 4, 4, 4, 5   // In nine: 36
@@ -37,19 +32,16 @@ public class StrokePlayTest {
         Byte[] coursePlayerB = {
             4, 4, 4, 4, 4, 4, 3, 4, 5,  // Out nine: 36
             4, 4, 4, 4, 4, 4, 4, 4, 5   // In nine: 37
-        }; // Total: 73
+        };
         
-        // Añadir recorridos al course
         course.addPlayerCourse(playerA, coursePlayerA);
         course.addPlayerCourse(playerB, coursePlayerB);
         
-        // Establecer el course en la scoreCard
         scoreCard.setPlayersCourse(course);
     }
 
     @Test
     void testScoring() {
-        // si jugamos stroke play
         strokePlay.scoring(scoreCard);
         assertEquals((short) 71, playerA.getScore());
         assertEquals((short) 73, playerB.getScore());
